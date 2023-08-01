cb388f7ab182347a4c6b98e046a59b15a0e7862d
herintsaina <herintsainarak024@gmail.com>
2023-08-01 18:46:44 +0300

    dashboard mockup Vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- <registration-home/> -->
+    <home-banner/>
+  </div>
+</template>
+
+<script>
+// import RegistrationHome from './components/RegistrationForm.vue'
+import HomeBanner from './components/HomeBanner.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // RegistrationHome,
+    HomeBanner,
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="HomeBanner">
+    <header>
+        logo
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
